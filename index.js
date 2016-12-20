199c0f61f6f36c2d9489e4b6f48aea0b53e54298
Hashemi Rafsan <rafsanhashemi@gmail.com>
2016-12-21 00:03:32 +0600

    changes

--- index.js
+++ index.js
@@ -5,7 +5,25 @@
 
 'use strict'
 
+var nimoPoly = {
+  navlist : "",   // nav bar list
+  /*
+    Url params checking function
+  */
+  check : function (nav){
+    var VerifyStringNav = nav.replace(/[`@()!'~$*^%"#:;.,{}_|]/gi,"");
+    for (var i = 0; i < this.navlist.length; i++) {
+      if(this.navlist[i] === VerifyStringNav){
+        return VerifyStringNav;
+        break;
+      } else{
+        return "404";
+      }
+    }
+  }
+};
+
 /*
 * Export module
 */
-module.exports = require('./lib/nimoPoly');
+module.exports = nimoPoly
